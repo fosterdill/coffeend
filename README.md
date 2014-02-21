9d628cd1c06235314a50016de37634b31531149e
Dylan Foster <fosterdill@gmail.com>
2014-02-20 17:34:16 -0800

    add readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+###Coffeend
+
+Everyone loves to get coffee right? I like getting coffee with people I don't know yet and building my network. This is why I want to make Coffeend. Put up a time and place you are getting coffee, and maybe someone else wants to get it with you. Why not? 
+
+###Status
+
+In development.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeend::Application.config.secret_token = '9a503d12d6227d39e3ae976c9f2a1fe03167f820076537e04b6b1bbcee73ed7bf925c9ef624b6ceb9fc6634f2bfdf5719ea62f4257bb0cdeb8772e13bc70af6f'
